1ba4d5cb38e5d0cfae5fc3fb3f8209ba1f47d036
grozamorei <desperatebassplayer@gmail.com>
2017-07-17 18:23:25 +0300

    set debug port to comply to new convention

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const connect = require('gulp-connect')
 gulp.task('connect', () => {
     connect.server({
         root: 'build/',
-        port: '8080',
+        port: '8100',
         livereload: true
     })
 })
