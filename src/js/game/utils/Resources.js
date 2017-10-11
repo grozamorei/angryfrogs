ed4f70a870008f42b0e4a91c022995259d6766ad
grozamorei <desperatebassplayer@gmail.com>
2017-10-11 12:10:06 +0300

    level load fix + error logging

--- src/js/game/utils/Resources.js
+++ src/js/game/utils/Resources.js
@@ -1,17 +1,27 @@
 
 export const Resources = () => {
 
+    const loader = PIXI.loader
+    const res = loader.resources
+
     const self = {
         add: (alias, path) => {
-            PIXI.loader.add(alias, path)
+            loader.add(alias, path)
             return self
         },
         load: (onComplete) => {
-            PIXI.loader.load(onComplete)
+            loader.load(onComplete)
             return self
         },
-        getTexture: (alias) => PIXI.loader.resources[alias] ? PIXI.loader.resources[alias].texture : PIXI.loader.resources.pixel.texture,
-        getJSON: (alias) => PIXI.loader.resources[alias].data
+        getTexture: (alias) => {
+            if (alias in res) return res[alias].texture
+            // console.warn('texture', alias, 'was replaced with default texture')
+            return res.pixel.texture
+        },
+        getJSON: (alias) => {
+            if (alias in res) return res[alias].data
+            throw '   Cannot find JSON with name ' + alias
+        }
     }
     return self
 }
