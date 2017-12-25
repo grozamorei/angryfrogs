434e16037e18c04e617248962c35818ee08a2fe1
grozamorei <desperatebassplayer@gmail.com>
2017-12-25 19:52:18 +0300

    gulp will create miniatures for level pieces now

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task('pack-digests', ['clean', 'deploy-static'], () => {
 
     const buildDigest = (assetPath, assetType) => {
         const digest = []
-        gulp.src(['build/assets/' + assetPath + '/**/*.' + assetType])
+        return gulp.src(['build/assets/' + assetPath + '/**/*.' + assetType])
             .pipe(through.obj(
                 (ch, enc, cb) => {
                     const loadPath = path.relative(path.join(ch.cwd, 'build'), ch.path)
@@ -86,12 +86,52 @@ gulp.task('pack-digests', ['clean', 'deploy-static'], () => {
                 })).pipe(through.obj())
     }
 
-    buildDigest('patterns', 'json')
     buildDigest('art', 'png')
     buildDigest('shaders', 'glsl')
+    return buildDigest('patterns', 'json')
 })
 
-gulp.task('deploy', ['clean', 'pack', 'pack-css', 'deploy-static', 'pack-digests'], () => {
+gulp.task('create-pattern-bitmaps', ['pack-digests'], () => {
+    const toHex = (hex) => {
+        if (hex.length === 9) {
+            hex = '#' + hex.substr(3)
+        }
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+            return r + r + g + g + b + b;
+        });
+
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return parseInt(result[1], 16) << 16 | parseInt(result[2], 16) << 8 | parseInt(result[3], 16)
+    }
+
+    const fs = require('fs')
+    fs.mkdir('build/assets/debug')
+
+    const path = require('path')
+    const bm = require('bitmap-manipulation')
+
+    const patterns = JSON.parse(fs.readFileSync('build/assets/patterns/digest.json'))
+    patterns.forEach(p => {
+        const mapFile = JSON.parse(fs.readFileSync(path.join('build', p.path)))
+        const w = mapFile.width * mapFile.tilewidth
+        const h = mapFile.height * mapFile.tileheight
+
+        const bmp = new bm.BMPBitmap(Math.floor(w/10), Math.floor(h/10))
+        mapFile.layers.forEach(layer => {
+            const clr = layer.color
+            layer.objects.forEach(obj => {
+                bmp.drawFilledRect(Math.floor(obj.x / 10), Math.floor(obj.y / 10),
+                    Math.floor(obj.width/10), Math.floor(obj.height/10), 0x00, toHex(clr))
+            })
+        })
+
+        bmp.save('build/assets/debug/' + p.alias + '.bmp')
+    })
+})
+
+gulp.task('deploy', ['clean', 'pack', 'pack-css', 'deploy-static', 'pack-digests', 'create-pattern-bitmaps'], () => {
     gulp.src(['src/**/*']).pipe(connect.reload());
 });
 
@@ -100,3 +140,11 @@ gulp.task('watch', () => {
 });
 
 gulp.task('default', ['connect', 'deploy', 'watch'])
+
+gulp.task('cache-bitmap', () => {
+    const bm = require('bitmap-manipulation')
+    const bitmap = new bm.BMPBitmap(400, 300)
+    // bitmap.drawFilledRect(10, 10, 100, 100, 0x00, 0xFF)
+
+    // bitmap.save('build/test.bmp')
+})
